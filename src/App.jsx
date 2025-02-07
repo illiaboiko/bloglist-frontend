0a288b9c2b2549ee5fc6fcac2deaf4c85c492116
illiaboiko <illiaboiko.095@gmail.com>
2025-02-07 19:05:56 +0200

    completed exercise 5.5

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Blog from "./components/Blog";
+import BlogForm from "./components/BlogForm";
 import Notification from "./components/Notification";
 import blogService from "./services/blogs";
 import loginService from "./services/login";
+import Toggable from "./components/Toggable";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [user, setUser] = useState(null);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [title, setTitle] = useState("");
-  const [author, setAuthor] = useState("");
-  const [url, setUrl] = useState("");
-  const [notification, setNotification] = useState(null)
+
+  const [notification, setNotification] = useState(null);
+
+  const blogFormRef = useRef(null);
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs([...blogs].reverse()));
-  }, []);
+  }, [blogs]);
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem("loggedBlogAppUser");
@@ -38,23 +40,23 @@ const App = () => {
       setPassword("");
       setNotification({
         text: "Login Successful!",
-        type: "success"
-      })
+        type: "success",
+      });
 
-       setTimeout(() => {
+      setTimeout(() => {
         setNotification(null);
       }, 5000);
     } catch (exception) {
       if (exception.response && exception.response.data) {
         setNotification({
           text: exception.response.data.error,
-          type: "error"
-        })
+          type: "error",
+        });
       } else {
         setNotification({
           text: "Login Failed. Please try again",
-          type: "error"
-        })
+          type: "error",
+        });
       }
 
       setTimeout(() => {
@@ -68,39 +70,30 @@ const App = () => {
     setUser(null);
   };
 
-  const handleCreateBlog = async (event) => {
-    event.preventDefault();
-    const newBlogObj = {
-      title,
-      author,
-      url,
-    };
-
+  const handleCreateBlog = async (newBlogObj) => {
+    blogFormRef.current.toggleVisibility();
     try {
       await blogService.createBlog(newBlogObj);
-      setTitle("")
-      setAuthor("")
-      setUrl("")
-      
+
       setNotification({
         text: "New blog added!",
-        type: "success"
-      })
+        type: "success",
+      });
 
-       setTimeout(() => {
+      setTimeout(() => {
         setNotification(null);
       }, 5000);
     } catch (exception) {
       if (exception.response && exception.response.data) {
         setNotification({
           text: exception.response.data.error,
-          type: "error"
-        })
+          type: "error",
+        });
       } else {
         setNotification({
           text: "Failed to create blo. Try again",
-          type: "error"
-        })
+          type: "error",
+        });
       }
 
       setTimeout(() => {
@@ -137,58 +130,22 @@ const App = () => {
     );
   };
 
-  const createFrom = () => (
-    <>
-      <form onSubmit={handleCreateBlog}>
-        <p>new blog:</p>
-        <div>
-          <label htmlFor="title">title</label>
-          <input
-            onChange={({ target }) => setTitle(target.value)}
-            type="text"
-            name="title"
-            id="title"
-            value={title}
-          />
-        </div>
-        <div>
-          <label htmlFor="author">author</label>
-          <input
-            onChange={({ target }) => setAuthor(target.value)}
-            type="text"
-            name="author"
-            id="author"
-            value={author}
-          />
-        </div>
-        <div>
-          <label htmlFor="url">url</label>
-          <input
-            onChange={({ target }) => setUrl(target.value)}
-            type="text"
-            name="url"
-            id="url"
-            value={url}
-          />
-        </div>
-        <button type="submit">create</button>
-      </form>
-      <br />
-    </>
-  );
-
   return (
     <div>
       <div>
         <h2>blogs</h2>
-        {notification && <Notification text={notification.text} type={notification.type} />}
+        {notification && (
+          <Notification text={notification.text} type={notification.type} />
+        )}
         {user !== null ? (
           <div>
             <p>
               user <strong>{user.username}</strong> logged in
             </p>
             <button onClick={handleLogout}>LogOut</button>
-            {createFrom()}
+            <Toggable buttonLabel="New Blog" ref={blogFormRef}>
+              <BlogForm createBlog={handleCreateBlog} />
+            </Toggable>
           </div>
         ) : (
           loginForm()
