3a6902736d503d8f0bb621618c6658abea2a336f
illiaboiko <illiaboiko.095@gmail.com>
2025-02-07 20:10:48 +0200

    completed exercises 5.6-5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,9 +1,43 @@
-const Blog = ({ blog }) => (
-  <div>
-    <a href={blog.url}><strong>{blog.title}</strong></a>   by {blog.author}
-     <br />
-     {blog.likes} likes
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const toggleView = () => {
+    setVisible(!visible);
+  };
+
+  const additionalInfo = () => {
+    return (
+      <>
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            {blog.likes} likes
+            <button>like</button>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} by {blog.author}
+        <button onClick={toggleView}>{visible ? "hide": "view"}</button>
+        {visible && additionalInfo()}
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
